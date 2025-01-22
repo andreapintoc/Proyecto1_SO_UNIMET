5163c5322109672d728f751b02e902189e88ed9d
andreapintoc <77283293+andreapintoc@users.noreply.github.com>
2025-01-22 09:27:10 -0400

    Create main.java (first commit)

---
+++ main.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) throws InterruptedException {
+        
+        VideoStart v= new VideoStart();
+        v.setVisible(true);
+
+        w  = new Window(); 
+    }
